2573810674c7062167f598e4602bf8c21309aaed
Ishmael Adebisi <isvara99@isvara-dass-imac.local>
2018-06-04 16:42:00 +0530

    halting till i learn more, it is set up tho, swithcing to bitbucket i guess to remain private REEEE

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,21 @@
+//Requires
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+//APP SETUP
 const app = express()
-
 app.use(cors())
 app.use(morgan('combined'))
 app.use(bodyParser.json())
-
+//INITIAL ROUTES
 app.get('/', (req, res) => {
-    res.send({
-        message: 'Lol'
-    })
+    res.send('reeee')
 })
-
+app.post('/register', (req, res) =>{
+    res.send('Hello ${req.body.email} was registered!')
+})
+//SERVER STATUS
 app.listen(process.env.PORT || 8081, () => {
     console.log('Server Started')
 })
